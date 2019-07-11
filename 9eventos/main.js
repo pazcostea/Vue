ccf5aeac6fa4815d52be1677b8a23ebe13d15265
pazcostea <pazcostea@gmail.com>
2019-07-11 14:40:05 +0200

    Eventos

---
+++ 9eventos/main.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", () => 
+{
+    console.log("Ventana cargada correctamente");
+    
+   
+    let aniadirTarea  = document.getElementById('aniadirTarea');
+    let inputTarea = document.getElementById('nombreTarea');
+    let lista = document.getElementById('tareas');
+
+    aniadirTarea.addEventListener('click', ()=> {
+        let nombreTarea = inputTarea.value; // tomar valor del input
+
+        let li = document.createElement('li'); // <li> </li>
+        li.innerText = nombreTarea; // <li> nombre tarea </li>
+        lista.appendChild(li); // añadir el li a la lista
+
+        inputTarea.value = ""; // resetear valor del input
+    })
+});
