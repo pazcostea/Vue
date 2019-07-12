050b3a9c7cb2bc39ffcd89dda95cbd6182adf89b
pazcostea <pazcostea@gmail.com>
2019-07-12 10:32:39 +0200

    Primeras pruebas vue

---
+++ todos/src/views/todos.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{nombre}}</h1>
+    <h3>{{numero}}</h3>
+    <button @click="incrementar">Incrementar</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      nombre: 'Hola mundo',
+      numero: 0,
+    };
+  },
+  methods: {
+    incrementar() {
+      this.numero += 1;
+    },
+  },
+};
+
+</script>
